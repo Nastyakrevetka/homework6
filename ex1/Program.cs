d789d4f07600a8f8234a9fc575a4133554eaccc0
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-29 17:03:12 +0300

    задача1 колическтво чисел больше 0

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите элементы массива: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов > 0: {count}");
